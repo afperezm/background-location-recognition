2279953ac71ee617b19a19a9eabb924d320150c3
Andres Perez <andresf@echoes.(none)>
2013-07-08 14:34:24 +0200

    scripts/thumbnailer.sh * Identated code.

--- scripts/thumbnailer.sh
+++ scripts/thumbnailer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [ "$#" -ne "2" ]; then
-echo "Usage: $0 <images.folder> <output.thumbs.folder>"
-exit 1
+	echo "Usage: $0 <images.folder> <output.thumbs.folder>"
+	exit 1
 fi
 
 for file in $1/*.jpg
 do
-filename=$(basename $file)
-filename="${filename%.*}"
-echo "Creating thumbnail for file [$file]"
-/usr/bin/convert -thumbnail 200 $file $2/$filename.thumb.jpg
+	filename=$(basename $file)
+	filename="${filename%.*}"
+	echo "Creating thumbnail for file [$file]"
+	/usr/bin/convert -thumbnail 200 $file $2/$filename.thumb.jpg
 done
+
