abacc0c40ca98a84f0a6adbd1fbc6e5adf99960b
Andres Perez <andresf@echoes.(none)>
2013-07-10 11:47:43 +0200

    scripts/plot_performance_rates.m * Idented code.

--- scripts/plot_performance_rates.m
+++ scripts/plot_performance_rates.m
@@ -1,5 +1,4 @@
-function plot_performance_rates(recall_rates, precision_rates, line_format='r-o')
-% function plot_performance_rates(recall_rates, precision_rates, line_format='r-o')
+% plot_performance_rates(recall_rates, precision_rates, line_format='r-o')
 %
 % This function plots the average precision and recall rates computed previously
 % starting from the matrix of right landmark ids occurrences.
@@ -10,9 +9,17 @@ function plot_performance_rates(recall_rates, precision_rates, line_format='r-o'
 %	line_format: format of the ploted curves
 %
 
-% Plot avg precision and recall
-subplot(3,1,1), hold on, plot(1:length(recall_rates), recall_rates, line_format), hold off, xlabel('Number of candidates'), ylabel('Recall');
-subplot(3,1,2), hold on, plot(1:length(precision_rates), precision_rates, line_format), hold off, xlabel('Number of candidates'), ylabel('1-Precision');
-subplot(3,1,3), hold on, plot(precision_rates, recall_rates, line_format), hold off, xlabel('1-Precision'), ylabel('Recall');
+function plot_performance_rates(recall_rates, precision_rates, color)
+
+	if nargin < 3
+	    color = 'r';
+	end
+
+	line_format = strcat(color, '-d');
+
+	% Plot avg precision and recall
+	subplot(3,1,1), hold on, plot(1:length(recall_rates), recall_rates, line_format, 'MarkerFaceColor', color, 'MarkerSize', 3), hold off, xlabel('Number of candidates'), ylabel('Recall');
+	subplot(3,1,2), hold on, plot(1:length(precision_rates), precision_rates, line_format, 'MarkerFaceColor', color, 'MarkerSize', 3), hold off, xlabel('Number of candidates'), ylabel('1-Precision');
+	subplot(3,1,3), hold on, plot(precision_rates, recall_rates, line_format, 'MarkerFaceColor', color, 'MarkerSize', 3), hold off, xlabel('1-Precision'), ylabel('Recall');
 
 end
