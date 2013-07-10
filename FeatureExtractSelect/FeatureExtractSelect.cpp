ff9eea3f483e0468c8b1b82905b0854e1fc368a4
Andres Perez <andresf@echoes.(none)>
2013-07-10 20:14:23 +0200

    Fixed number of parameters to show the usage text

--- FeatureExtractSelect/FeatureExtractSelect.cpp
+++ FeatureExtractSelect/FeatureExtractSelect.cpp
@@ -29,7 +29,7 @@ void writeFeaturesToFile(string& outputFilepath, Features& keypoints);
 
 int main(int argc, char **argv) {
 
-	if (argc < 8) {
+	if (argc != 4 && argc != 5) {
 		printf(
 				"\nUsage:\n"
 						"\t%s -cf <in.imgs.folder> <out.keys.folder> {in.startimg.id}\n\n"
